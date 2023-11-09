f2a8a7a42c360ce0964a2cf46c4152f6ca52fefa
Akram Abuhajar <99180753+AkramNabh@users.noreply.github.com>
2023-11-09 22:56:06 +0200

    Modification V1.6

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,13 +9,13 @@ const Skills = async (props: Props) => {
 
   const skillData:Skill[]=await getSkill();
   return (
-    <section className='h-screen p-5 max-w-7xl mx-auto flex flex-col justify-center items-center space-y-24'>
+    <section className='h-screen max-w-7xl mx-auto flex flex-col justify-center items-center space-y-24'>
         <h1 className='text-2xl uppercase tracking-[20px] text-[#007CBE]'>Skills</h1>
         <Motion
         delay={1}
         direction={''}
         >
-        <div className='grid grid-cols-3 md:grid-cols-6 gap-4 md:gap-8'>
+        <div className='grid grid-cols-3 md:grid-cols-8 gap-4 md:gap-8'>
             {skillData.map((s:Skill,index:number)=>(
               <SkillCircle key={index} props={s}/>
             ))}
