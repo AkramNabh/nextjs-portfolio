397d373417f5554439de70859e8d363c18e7aff8
Akram Abuhajar <99180753+AkramNabh@users.noreply.github.com>
2023-11-09 23:03:44 +0200

    Modification V1.7

--- src/components/Learning.tsx
+++ src/components/Learning.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Learning = ({props}: Props) => {
   return (
-    <Link href={"/learnings/"+props._id}className='h-[200px] relative rounded-lg group cursor-pointer shadow-lg shadow-[#1E1B18] hover:scale-105 duration-300'>
+    <Link href={"/learnings/"+props._id}className='h-[150px] relative rounded-lg group cursor-pointer shadow-lg shadow-[#1E1B18] hover:scale-105 duration-300'>
     <Image src={props.MainImage} alt='gravafun' className='w-full h-full rounded-lg' height={150} width={200}/>
 <div className='bg-[#FF7F11] absolute top-0 left-0 h-full w-full flex justify-center items-center text-[#F9F8F8] p-4 text-xs text-center rounded-lg opacity-0 group-hover:opacity-100 transition-all ease-in duration-300'>
 {props.description}
--- src/components/SkillCircle.tsx
+++ src/components/SkillCircle.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const SkillCircle = ({props}: Props) => {
   return (
-    <div className='h-24 w-24 flex justify-center items-center p-1 shadow-lg shadow-[#1E1B18] bg-[#007CBE] hover:bg-[#FF7F11] hover:scale-110 duration-300 rounded-full '>
+    <div className='h-20 w-20 flex justify-center items-center p-1 shadow-lg shadow-[#1E1B18] bg-[#007CBE] hover:bg-[#FF7F11] hover:scale-110 duration-300 rounded-full '>
         <div className='flex justify-center items-center bg-[#F9F8F8] rounded-full h-full w-full'>
             <Image src={props.image} alt='skill' width={50} height={50} className=''/>
         </div>
