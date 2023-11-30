ec96c84d8cab92dcd65c95d6f3df1099254daf83
Akram Abuhajar <99180753+AkramNabh@users.noreply.github.com>
2023-11-30 15:49:25 +0200

    Dark-Light Mode Fix

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,36 +1,47 @@
-"use client"
-import { useTheme } from 'next-themes'
-import React, { useEffect, useState } from 'react'
-import { SunIcon, MoonIcon } from '@heroicons/react/20/solid'
-import Motion from './Motion'
-type Props = {}
+"use client";
+import { useTheme } from "next-themes";
+import React, { useEffect, useState } from "react";
+import { SunIcon, MoonIcon } from "@heroicons/react/20/solid";
+import Motion from "./Motion";
+type Props = {};
 
 const ThemeButton = (props: Props) => {
-    const {theme, setTheme} = useTheme();
-    const [mounted, setMounted] = useState(false);
-    useEffect(() => setMounted(true), [])
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    // Check local storage for the saved theme preference
+    const savedTheme = localStorage.getItem("theme");
 
-    if(!mounted){
-        return null
-    }
+    // Set the theme based on local storage or a default value (e.g., 'light')
+    setTheme(savedTheme || "light");
+
+    // Component is now mounted
+    setMounted(true);
+  }, [setTheme]);
+
+  if (!mounted) {
+    return null;
+  }
   return (
-    <Motion
-    delay={1}
-    direction={''}
-    >
-    <div className='fixed md:bottom-16 md:right-20 z-50 right-8 bottom-10'>
-    <button 
-    className={`md:px-5 md:py-5 px-3 py-3 rounded-full hover:duration-300 text-lg ${
-        theme === 'dark' ? 'bg-[#003566] hover:bg-[#007CBE]' : 'bg-[#007CBE] hover:bg-[#003566]'
-      } duration-300`}
-    onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-    >
-    {theme === 'dark' ? <SunIcon className='h-5 w-5 text-orange-300' /> 
-    : <MoonIcon className='h-5 w-5 text-slate-800' />}
-  </button>
-  </div>
-  </Motion>
-  )
-}
+    <Motion delay={1} direction={""}>
+      <div className="fixed md:bottom-16 md:right-20 z-50 right-8 bottom-10">
+        <button
+          className={`md:px-5 md:py-5 px-3 py-3 rounded-full hover:duration-300 text-lg ${
+            theme === "dark"
+              ? "bg-[#003566] hover:bg-[#007CBE]"
+              : "bg-[#007CBE] hover:bg-[#003566]"
+          } duration-300`}
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          {theme === "dark" ? (
+            <SunIcon className="h-5 w-5 text-orange-300" />
+          ) : (
+            <MoonIcon className="h-5 w-5 text-slate-800" />
+          )}
+        </button>
+      </div>
+    </Motion>
+  );
+};
 
-export default ThemeButton
+export default ThemeButton;
