be44a5b920ddef7d65bec253eb9d26d54911216c
Akram Abuhajar <99180753+AkramNabh@users.noreply.github.com>
2023-12-01 15:12:49 +0200

    Favicon Modification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,43 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import Provider from './providers/Provider'
+import "./globals.css";
+import type { Metadata } from "next";
+import Provider from "./providers/Provider";
 export const metadata: Metadata = {
-  title: 'Akram`s | Portfolio',
-  description: 'Akram Abuhajar Works and Contacts',
-}
+  title: "Akram`s | Portfolio",
+  description: "Akram Abuhajar Works and Contacts",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon-192x192.png"
+        />
+      </head>
       {/* <body>{children}</body> */}
-      <body><Provider>{children}</Provider></body>
+      <body>
+        <Provider>{children}</Provider>
+      </body>
     </html>
-  )
+  );
 }
